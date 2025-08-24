0329ba67043c85ec410921deb49923c52b0b9c08
alexmkennell-ship-it <alexmkennell@gmail.com>
2025-08-24 15:41:54 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-// Simple offline cache
-const CACHE = 'trader-prep-v1';
+// Simple offline cache (bump version when you change HTML)
+const CACHE = 'trader-prep-v2';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.webmanifest'
+  './manifest.webmanifest',
+  './sw.js'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
